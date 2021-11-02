d169339116d298f9637cd8528df5871f45b8a8a1
shpark <bingbingpa@gmail.com>
2021-11-02 22:20:03 +0900

    feat: init

---
+++ user/src/test/java/me/bingbingpa/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bingbingpa.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
